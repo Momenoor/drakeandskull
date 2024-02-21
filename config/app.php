2515a81b9d4013b16410d43f22a2aba8e8af6707
Momen Noor <momen.noor@gmail.com>
2024-02-22 00:24:02 +0400

    Initial Commit

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        GrofGraf\LaravelPDFMerger\Providers\PDFMergerServiceProvider::class,
+        //GrofGraf\LaravelPDFMerger\Providers\PDFMergerServiceProvider::class,
     ])->toArray(),
 
     /*
